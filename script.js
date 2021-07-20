7168070b2e1126dab12421e689903937d8c9a4e7
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-07-20 23:10:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let questions = [{
+"question": "Wie nennt man den Text in dem Computerprogramme geschrieben werden?",
+"answer_1": "Geheimtext",
+"answer_2": "Ausführungstext",
+"answer_3": "Funktionstext",
+"answer_4": "Quelltext",
+"right_answer": 4
+},
+{
+    "question": "Welche Programmiersprache schreibt man wie ein Kochrezept?",
+    "answer_1": "Chef",
+    "answer_2": "Gourmet",
+    "answer_3": "Dessert",
+    "answer_4": "Cake",
+    "right_answer": 1
+}
+]
