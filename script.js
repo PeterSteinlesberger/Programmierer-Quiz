4aa443e50a009d34d4f3da116d1282c36a342c94
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-07-21 00:18:03 +0200

    JSON Array added

--- script.js
+++ script.js
@@ -13,5 +13,84 @@ let questions = [{
     "answer_3": "Dessert",
     "answer_4": "Cake",
     "right_answer": 1
-}
-]
+},
+{
+    "question": "Was ist die jüngste Programmiersprache?",
+    "answer_1": "Swift",
+    "answer_2": "AppleScript",
+    "answer_3": "Java",
+    "answer_4": "Amiga E",
+    "right_answer": 1
+},
+{
+    "question": "Wann wurde die Programmiersprache JavaScript eingeführt?",
+    "answer_1": "1982",
+    "answer_2": "2002",
+    "answer_3": "1995",
+    "answer_4": "1964",
+    "right_answer": 3
+},
+{
+    "question": "Was ist keine Programmiersprache?",
+    "answer_1": "PHP",
+    "answer_2": "Zoom 4",
+    "answer_3": "C++",
+    "answer_4": "Pascal",
+    "right_answer": 2
+},
+{
+    "question": "Was ist kein Content Management System (CMS)?",
+    "answer_1": "cURL",
+    "answer_2": "Joomla",
+    "answer_3": "WordPress",
+    "answer_4": "Typo3",
+    "right_answer": 1
+},
+{
+    "question": "In welche Jahr wurde die erste höhere Programmiersprache Plankalkül eingeführt?",
+    "answer_1": "1946",
+    "answer_2": "1961",
+    "answer_3": "1954",
+    "answer_4": "1958",
+    "right_answer": 1
+},
+{
+    "question": "An welcher Sprache orientieren sich die meisten Programmiersprachen?",
+    "answer_1": "Deutsch",
+    "answer_2": "Englisch",
+    "answer_3": "Spanisch",
+    "answer_4": "Französisch",
+    "right_answer": 2
+},
+{
+    "question": "Welche Maßeinheit aus der Digitaltechnik gibt es nicht?",
+    "answer_1": "Terabyte",
+    "answer_2": "Megabyte",
+    "answer_3": "Petabyte",
+    "answer_4": "Zulerbyte",
+    "right_answer": 4
+},
+{
+    "question": "Was ist keine Spiele Engine?",
+    "answer_1": "Wall Engine",
+    "answer_2": "Unreal Engine",
+    "answer_3": "Cry Enginet",
+    "answer_4": "Source Eninge",
+    "right_answer": 1
+},
+{
+    "question": "Wobei handelt es sich um eine Programmiersprache?",
+    "answer_1": "Queens",
+    "answer_2": "Westminster",
+    "answer_3": "Hollywood",
+    "answer_4": "Spandau",
+    "right_answer": 3
+},
+{
+    "question": "Wofür steht die Abkürzung HTML?",
+    "answer_1": "Hypertext Message Language",
+    "answer_2": "Hypertext Meta Language",
+    "answer_3": "Hypertext Markup Language",
+    "answer_4": "Hypertext Model Language",
+    "right_answer": 3
+}]
